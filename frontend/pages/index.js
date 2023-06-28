b50bd32228e6a956f580cd535e2548302b92f449
Owoeye precious <owoeyeprecious36@gmail.com>
2023-06-28 20:03:28 +0100

    connected frontend with metamask

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,16 @@
 import {useState, useEffect} from "react";
 import PrimaryButton from "../components/primary-button";
-
+import abi from "../utils/Voting.json";
+import { Input } from "postcss";
+import { ethers } from "ethers";
 
 export default function Home() {
     const [ethereum ,setEthereum] = useState(undefined);
     const [connectedAccount, setConnectedAccount] = useState(undefined);
+    const[votersName, addVotersName] = useState("");
+    const[candidateParty, setCandidateParty] = useState("");
+    const connectedAddress = "0x7052066dffe6281011fb555d5f71ea11a5a13065";
+    const contractAbi = abi.abi;
 
     const handleAccounts = (accounts) => {
         if(accounts.length > 0) {
@@ -28,16 +34,45 @@ export default function Home() {
 
     useEffect(()=> getConnectedAccount(),[]);
 
-    const connectAccount = () => { };
-    if(!ethereum){
-      return <p>Please install MetaMask to connect to this site</p>;
-    }
+    const connectAccount = async () => { 
+      if(!ethereum){
+        alert("MetaMask is required to connect an account");
+        return ;
+      }
+      const accounts = await ethereum.request({method : 'eth_requestAccounts'});
+      handleAccounts(accounts);
+    };
+   
 
     if(!connectedAccount){
       return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>;
     }
 
-    return <p>connected Account: {connectedAccount}</p>
+    const addVoters = async (e)=> {
+      e.preventDefault();
+      if (!ethereum) {
+        console.error('Ethereum object is required to create a keyboard');
+        return;
+      }
+      const provider = new ethers.provider.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const votingContract = new ethers.Contract(connectedAddress, contractAbi, signer);
+      const createTxn = await votingContract.addCandidates(votersName, candidateParty); 
+      
 
 
-} 
+    }
+
+    return (<div>
+      <p>connected Account: {connectedAccount}</p>
+      <form>
+      <label>Add Candidate's name</label>
+      <input type="text" name="add-voters-name" value={votersName} onChange={(e)=> {addVotersName(e.target.value)}}/>
+      <br/>
+      <label>Add Candidate's party</label>
+      <input type="text" name="add-candidate-party" value={candidateParty} onChange={(e)=> {setCandidateParty(e.target.value)}}/>
+      <PrimaryButton type="submit" onClick={addVoters}>Add name</PrimaryButton>
+      </form>
+      </div>
+
+)} 
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.18",
   networks: {
+    
     sepolia: {
       url: QUICKNODE_HTTP_URL,
       chainId: 11155111,
